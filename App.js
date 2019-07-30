4642d23ca96f228639507770c6f6c7a4c7d266cf
raechel <odom.raechel@gmail.com>
2019-07-30 14:23:13 -0600

    bottom tab nav bar now features icons and animations and shows new pages

--- App.js
+++ App.js
@@ -1,19 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Marvelously</Text>
+      </View>
+    );
+  }
 }
+
+class ComicsScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Comics!</Text>
+      </View>
+    )
+  }
+}
+
+class MoviesScreen extends Component {
+  render() {
+   return (
+    <View>
+      <Text>Movies!</Text>
+    </View>
+   )
+  }
+}
+
+class UserScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Users!</Text>
+      </View>
+    )
+  }
+}
+
+const TabNavigator = createMaterialBottomTabNavigator({
+  Home: { screen: HomeScreen, navigationOptions: {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({ tintColor }) => (
+      <View>
+        <Icon style={[{color: tintColor}]} size={25} name={'ios-home'} />
+      </View>
+    )
+  } },
+  Comics: { screen: ComicsScreen, navigationOptions: {
+    tabBarLabel: 'Comics',
+    tabBarIcon: ({ tintColor }) => (
+      <View>
+        <Icon style={[{color: tintColor}]} size={25} name={'ios-book'} />
+      </View>
+    )
+  } },
+  Movies: { screen: MoviesScreen, navigationOptions: {
+    tabBarLabel: 'Movies',
+    tabBarIcon: ({ tintColor }) => (
+      <View>
+        <Icon style={[{color: tintColor}]} size={25} name={'ios-film'} />
+      </View>
+    )
+  } },
+  Users: { screen: UserScreen, navigationOptions: {
+    tabBarLabel: 'User',
+    tabBarIcon: ({ tintColor }) => (
+      <View>
+        <Icon style={[{color: tintColor}]} size={25} name={'md-person'} />
+      </View>
+    )
+  } }
+},
+{
+  initialRouteName: 'Home',
+  activeColor: '#f0edf6',
+  inactiveColor: 'brickred',
+  barStyle: { backgroundColor: 'red' },
+  
+})
+
+export default createAppContainer(TabNavigator);
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
